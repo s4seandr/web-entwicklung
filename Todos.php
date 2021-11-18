32899a58b1ec603289c95ec3ff71793bac78c579
s4seandr <74901675+s4seandr@users.noreply.github.com>
2021-11-18 16:54:30 +0100

    Add files via upload
    Uebung 1

---
+++ Todos.php
@@ -0,0 +1,91 @@
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Todos</title>
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <header class="jumbotron">
+            <center> <h1> Aufgabenplaner: Todos (Aktuelles Projekt)</h1></center>
+        </header>
+
+        <div class="row">
+
+            <div class="col-md-2">
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <a href=""> Login </a>
+                    </li>
+
+                    <li class="list-group-item">
+                        <a href=""> Projekte </a>
+                    </li>
+
+                    <li class="list-group-item">
+                        <a href=""> Aktuelles Projekt </a>
+                    </li>
+
+                    <div style="padding-left: 2em">
+                        <li class="list-group-item">
+                            <a href=""> Aufgaben </a>
+                        </li>
+                    </div>
+
+                    <div style="padding-left: 2em">
+                        <li class="list-group-item">
+                            <a href=""> Reiter </a>
+                        </li>
+                    </div>
+
+                    <div style="padding-left: 2em">
+                        <li class="list-group-item">
+                            <a href=""> Personen </a>
+                        </li>
+                    </div>
+                </ul>
+            </div>
+
+            <div class="col-md-10">
+                <div class="row">
+
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-header"> ToDo: </div>
+                            <ul class="list-group">
+                                <li class="list-group-item">HTML-Datei erstellen (Sebastian Andres)</li>
+                                <li class="list-group-item">CSS-Datei erstellen  (Janna Bartz)</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-header"> Erledigt: </div>
+                            <ul class="list-group">
+                                <li class="list-group-item">PC-eingeschaltet (Sebastian Andres)</li>
+                                <li class="list-group-item">Kaffee trinken   (Janna Bartz)</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-header"> Verschoben: </div>
+                            <ul class="list-group">
+                                <li class="list-group-item">Für die Uni lernen (Sebastian Andres)</li>
+                                <li class="list-group-item">WE-Übung erledigen (Janna Bartz)</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
